2c9626a17917d2ec07a791371868e2d07d61573b
mingng18 <92716196+mingng18@users.noreply.github.com>
2021-11-02 13:32:50 +0800

    Add files via upload

---
+++ Lab2/src/lab2/Lab2.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ming
+ */
+public class Lab2 {
+    public static void main(String[] args) {
+        
+        // Question 1
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integer number: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("\nEnter the operand: ");
+        char operand = sc.next().charAt(0);
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int rem = a % b;
+        
+        switch (operand){
+                case '+' :
+                    System.out.println("The addition is " + add);
+                    break;
+                    
+                case '-' :
+                    System.out.println("The subtraction is " + sub);
+                    break;
+                    
+                case '*' :
+                    System.out.println("The multiplication is " + mul);
+                    break;
+                    
+                case '/' :
+                    System.out.println("The division is " + div);
+                    break;
+                    
+                case '%' :
+                    System.out.println("The remainder is " + rem);
+                    break;
+                    
+                default:
+                    System.out.println("Invalid input");
+                    return;
+        }
+        
+        // Question 2
+        Random rand = new Random();
+        int num = rand.nextInt(6);
+        switch (num){
+                case 0:
+                    System.out.println("0 is zero");
+                    break;
+                    
+                case 1 :
+                    System.out.println("1 is one");
+                    break;
+                    
+                case 2 :
+                    System.out.println("2 is two");
+                    break;
+                    
+                case 3 :
+                    System.out.println("3 is three");
+                    break;
+                    
+                case 4 :
+                    System.out.println("4 is four");
+                    break;
+                    
+                case 5 :
+                    System.out.println("5 is five");
+                    break;
+                    
+                default:
+                    System.out.println("Invalid input");
+                    return;
+        }*/
+        
+        // Question 3
+        Scanner sc = new Scanner(System.in);
+        int com = sc.nextInt();
+
+        //double com = 0;
+        
+        /*if (sales <= 100){
+            com = sales * 0.05;
+        }else if (sales > 100 && sales <= 500){
+            com = sales * 0.075;
+        }else if (sales > 500 && sales <= 1000){
+            com = sales * 0.1;
+        }else if (sales > 1000){
+            com = sales * 0.125;
+        }*/
+        // System.out.println(com);
+        //System.out.printf("Commision is '%.2f'%n", com);
+                    
+    }
+    
+}
